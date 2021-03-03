602cba8b59c50ee7b032ea8470609c91e6e530b2
Leiviton Carlos <leiviton@ibm.com>
2021-03-03 15:57:59 -0300

    hr pay roll rest template

--- hr-payroll/src/main/java/com/leiviton/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/leiviton/hrpayroll/services/PaymentService.java
@@ -1,14 +1,32 @@
 package com.leiviton.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.leiviton.hrpayroll.entities.Payment;
+import com.leiviton.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemlate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Leiviton",200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		
+		uriVariables.put("id",""+workerId);
+		
+		Worker worker = restTemlate.getForObject(workerHost + "/workers/{id}",Worker.class, uriVariables);
+		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
 }
