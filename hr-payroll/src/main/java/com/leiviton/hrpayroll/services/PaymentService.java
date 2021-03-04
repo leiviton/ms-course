9469275b78c0df43ceb7469a9966450d150437b7
Leiviton Carlos <leiviton@ibm.com>
2021-03-04 11:17:29 -0300

    hr pay roll feign cloud

--- hr-payroll/src/main/java/com/leiviton/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/leiviton/hrpayroll/services/PaymentService.java
@@ -1,31 +1,21 @@
 package com.leiviton.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.leiviton.hrpayroll.entities.Payment;
 import com.leiviton.hrpayroll.entities.Worker;
+import com.leiviton.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
 	@Autowired
-	private RestTemplate restTemlate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
 		
-		uriVariables.put("id",""+workerId);
-		
-		Worker worker = restTemlate.getForObject(workerHost + "/workers/{id}",Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(),worker.getDailyIncome(), days);
 	}
 
