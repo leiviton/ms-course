6c41020ef6a3b7627518892a2f6ceb9265dffdc5
Leiviton Carlos <leiviton@ibm.com>
2021-03-04 13:35:25 -0300

    hr eureka server

--- hr-eurekaserver/src/main/java/com/leiviton/hreurekaserver/HrEurekaserverApplication.java
+++ hr-eurekaserver/src/main/java/com/leiviton/hreurekaserver/HrEurekaserverApplication.java
@@ -2,7 +2,9 @@ package com.leiviton.hreurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaserverApplication {
 
