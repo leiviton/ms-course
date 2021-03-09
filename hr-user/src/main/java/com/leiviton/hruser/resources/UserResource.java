ba0615ba508f402420d95dda602fbd40764b90fb
Leiviton Carlos <leiviton@ibm.com>
2021-03-09 09:55:21 -0300

    hr oauth create

--- hr-user/src/main/java/com/leiviton/hruser/resources/UserResource.java
+++ hr-user/src/main/java/com/leiviton/hruser/resources/UserResource.java
@@ -1,11 +1,6 @@
 package com.leiviton.hruser.resources;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
