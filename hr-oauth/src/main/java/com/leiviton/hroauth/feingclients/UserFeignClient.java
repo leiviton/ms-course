2df55b0ad4c7172c7ff1c37051408c2bd27cfcf0
Leiviton Carlos <leiviton@ibm.com>
2021-03-10 11:21:55 -0300

    hr oauth login jwt token

--- hr-oauth/src/main/java/com/leiviton/hroauth/feingclients/UserFeignClient.java
+++ hr-oauth/src/main/java/com/leiviton/hroauth/feingclients/UserFeignClient.java
@@ -1,4 +1,4 @@
-package com.leiviton.hroauth;
+package com.leiviton.hroauth.feingclients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
--- hr-oauth/src/main/java/com/leiviton/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/leiviton/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.leiviton.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.leiviton.hroauth.UserFeignClient;
 import com.leiviton.hroauth.entities.User;
+import com.leiviton.hroauth.feingclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
@@ -24,6 +27,18 @@ public class UserService {
 			throw new IllegalArgumentException("Email nao encontrado");
 		}
 		logger.info("email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		
+		if(user == null) {
+			logger.error("Email not found");
+			throw new UsernameNotFoundException("Email nao encontrado");
+		}
+		logger.info("email found: " + username);
 		return user;
 	}
 
